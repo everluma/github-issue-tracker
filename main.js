9cadce63460d3876d3d8e6cd879c34b1367d3d74
everluma <farjanaaktarmonisha@gmail.com>
2026-03-09 18:34:38 +0600

    Load API

--- main.js
+++ main.js
@@ -6,10 +6,48 @@ async function loadIssues(){
 
     const data = await res.json()
 
-    displayIssue(data.data)
+    displayIssues(data.data)
 }
 
 loadIssues()
 
 
 // Issue Card Display function 
+
+const container = document.getElementById("issuesContainer");
+
+function displayIssues(issues){
+    container.innerHTML = "";
+
+    issues.forEach(issue =>{
+        const card = document.createElement("div");
+
+        card.classList.add("issue-card");
+
+        card.innerHTML =`
+        <h3>${issue.title}</h3>
+
+        
+           <p>${issue.description}</p>
+
+           <p>Status: ${issue.status}</p>
+
+           <p>Author: ${issue.author}</p>
+
+           <p>Priority: ${issue.priority}</p>
+
+            <p>Laber: ${issue.label}</p>
+
+            <p>${issue.createdAt}</p>
+        `;
+
+        container.appendChild(card)
+    });
+}
+
+
+// open/ closed border 
+
+card.style.borderTop = issue.status === "open"
+?"5px solid green"
+:"5px solid purple";

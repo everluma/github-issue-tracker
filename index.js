0e9bebdd6d730de5fad9cdc9ac56f1c7ce79c1d6
everluma <farjanaaktarmonisha@gmail.com>
2026-03-09 14:38:30 +0600

    Login JS Added in this part

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('button');
 
 button.addEventListener('click', function (){
-    const username = document.querySelector("input[type='text']").value;
+    const username = document.getElementById('username').value;
 
-    const password = document.querySelector("input[type='password']").value;
+    const password = document.getElementById("password").value;
 
     if (username === 'admin' && password === 'admin123'){
     window.location.href= "main.html";
